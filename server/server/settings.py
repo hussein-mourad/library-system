38548c62b427feb71a7daf1b3651eab2faa9b622
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-05-05 19:03:15 +0300

    chore: updated app settings
    - Added myauth app
    - Added Pagination
    - Added jwt auth

--- server/server/settings.py
+++ server/server/settings.py
@@ -28,10 +28,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
 
 # Application definition
 
 INSTALLED_APPS = [
+    "myauth",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -39,7 +49,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "myauth",
     "library",
 ]
 
