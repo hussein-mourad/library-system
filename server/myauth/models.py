3f1248169ed465558385e3d6d08e9f460e2fe72d
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-05-05 19:04:07 +0300

    feat: added profile to myauth app

--- server/myauth/models.py
+++ server/myauth/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from library.utils import generate_filename
+from PIL import Image
 
 
 class User(AbstractUser):
@@ -12,15 +13,29 @@ class User(AbstractUser):
     ]
     role = models.CharField(max_length=10, choices=ROLES, default="borrower")
 
-    def __str__(self):
-        self.username
-
 
 class Profile(models.Model):
+    IMAGE_FIELD = "avatar"
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     address = models.TextField(blank=True)
     phone_number = models.CharField(max_length=15, blank=True)
+    avatar = models.ImageField(upload_to=generate_filename, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # save the profile first
+        super().save(*args, **kwargs)
+        # resize the image
+        if not self.avatar:
+            return
+        img = Image.open(self.avatar.path)
+        if img.height > 150 or img.width > 150:
+            output_size = (150, 150)
+            # create a thumbnail
+            img.thumbnail(output_size)
+            # overwrite the large image
+            img.save(self.avatar.path)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user.username}'s Profile"
--- server/myauth/views.py
+++ server/myauth/views.py
@@ -1,4 +1,4 @@
-from myauth.models import User
+from myauth.models import Profile, User
 from rest_framework import permissions, serializers, viewsets
 
 
@@ -18,10 +18,10 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ["id", "is_active", "date_joined"]
         extra_kwargs = {
             "username": {"required": True},
-            "role": {"required": True},
-            "first_name": {"required": True},
-            "last_name": {"required": True},
-            "email": {"required": True},
+            "role": {"required": False},
+            "first_name": {"required": False},
+            "last_name": {"required": False},
+            "email": {"required": False},
         }
 
 
@@ -46,3 +46,39 @@ class UserViewSet(viewsets.ModelViewSet):
         user = serializer.save()
         user.set_password(password)
         user.save()
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = [
+            "id",
+            "user",
+            "bio",
+            "address",
+            "phone_number",
+            "avatar",
+        ]
+        read_only_fields = ["id"]
+        extra_kwargs = {
+            "user": {"required": False},
+            "bio": {"required": False},
+            "address": {"required": False},
+            "phone_number": {"required": False},
+            "avatar": {"required": False},
+        }
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.role == "admin":
+            return Profile.objects.all()
+        else:
+            return Profile.objects.filter(id=self.request.user.id)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
