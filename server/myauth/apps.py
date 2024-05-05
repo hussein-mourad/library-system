b27831a5c27428c0368dbecb6ba08b0368d968c2
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-05-05 19:04:07 +0300

    feat: added profile to myauth app

--- server/myauth/apps.py
+++ server/myauth/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MyAuthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "myauth"
+
+    def ready(self):
+        import myauth.signals
--- server/myauth/urls.py
+++ server/myauth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import UserViewSet
+from .views import ProfileViewSet, UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
+router.register(r"profiles", ProfileViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
