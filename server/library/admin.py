049cbbb425ec803e905d0d83a1b3544cb5e578d4
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-05-05 19:00:46 +0300

    feat: added models to admin panel

--- server/library/admin.py
+++ server/library/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from library.models import Author, Book, Borrow, Category, Comment
 
-# Register your models here.
+admin.site.header = "Library Administration"
+admin.site.site_header = "Library Administration"
+admin.site.site_title = "Library Management System"
+admin.site.index_title = "Welcome to Library Admin Mangement System"
+admin.site.site_url = ""
+
+
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(Borrow)
+admin.site.register(Comment)
