c2fdfad0d945116e5450a02eb7610ffad8fd7ff8
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-05-05 19:02:38 +0300

    feat: added jwt auth

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,15 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import include, path
+from library.urls import router as library_router
+from myauth.urls import router as myauth_router
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView, TokenVerifyView)
+
+router = DefaultRouter()
+router.registry.extend(myauth_router.registry)
+router.registry.extend(library_router.registry)
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path("api/", include("myauth.urls")),
-    path("api/", include("library.urls")),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
 # Add media URL pattern
