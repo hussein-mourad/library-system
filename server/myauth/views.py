2e3fefc8ead79dd18d3279ef1072cdaee1561322
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-05-06 07:26:22 +0300

    refactor: move myauth serializers to their one file

--- server/myauth/views.py
+++ server/myauth/views.py
@@ -1,30 +1,7 @@
 from myauth.models import Profile, User
-from rest_framework import permissions, serializers, viewsets
+from myauth.serializers import ProfileSerializer, UserSerializer
+from rest_framework import permissions, viewsets
 
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = [
-            "id",
-            "username",
-            "role",
-            "first_name",
-            "last_name",
-            "email",
-            "is_active",
-            "date_joined",
-            "password",
-        ]
-        read_only_fields = ["id", "is_active", "date_joined"]
-        extra_kwargs = {
-            "username": {"required": True},
-            "role": {"required": False},
-            "first_name": {"required": False},
-            "last_name": {"required": False},
-            "email": {"required": False},
-            "password": {"write_only": True, "required": True},
-        }
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -64,22 +41,6 @@ class UserViewSet(viewsets.ModelViewSet):
             user.is_staff = False
             user.is_superuser = False
         user.save()
-
-
-class ProfileSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source="user.username", read_only=True)
-
-    class Meta:
-        model = Profile
-        fields = "__all__"
-        read_only_fields = ["id"]
-        extra_kwargs = {
-            "user": {"required": False},
-            "bio": {"required": False},
-            "address": {"required": False},
-            "phone_number": {"required": False},
-            "avatar": {"required": False},
-        }
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
