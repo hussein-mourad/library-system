fe032c8e4a590702e6837fa859f28cf9ed3fcf98
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-05-06 11:07:00 +0300

    refactor: splitted views and serializers

--- server/library/views.py
+++ server/library/views.py
@@ -1,44 +1,11 @@
 from django.utils.version import os
 from library.models import Author, Book, Borrow, Category, Comment
-from rest_framework import serializers, viewsets
+from library.serializers import (AuthorSerializer, BookSerializer,
+                                 BorrowSerializer, CategorySerializer,
+                                 CommentSerializer)
+from rest_framework import viewsets
 
 from server.mixins import BulkActionsMixin
-
-
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = "__all__"
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = "__all__"
-
-
-class BookSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(source="author.name")
-    category = serializers.StringRelatedField(source="category.name")
-    # To include the author and category details in the response, use the following code:
-    # author = AuthorSerializer()
-    # category = CategorySerializer()
-
-    class Meta:
-        model = Book
-        fields = "__all__"
-
-
-class BorrowSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Borrow
-        fields = "__all__"
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = "__all__"
 
 
 class AuthorViewSet(BulkActionsMixin, viewsets.ModelViewSet):
