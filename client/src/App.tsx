80fad7209c200b040205c301acd3ed886e9c3039
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-05-07 10:39:16 +0300

    feat: added dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,75 @@
 import "./App.css";
-import {
-  Admin,
-  EditGuesser,
-  ListGuesser,
-  Resource,
-  ShowGuesser,
-} from "react-admin";
+import { Admin, Resource } from "react-admin";
 import UserIcon from "@mui/icons-material/Group";
-import CommentIcon from '@mui/icons-material/Comment';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import CategoryIcon from '@mui/icons-material/Category';
-import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
-import BookOnlineIcon from '@mui/icons-material/BookOnline';
-import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
+import CommentIcon from "@mui/icons-material/Comment";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import CategoryIcon from "@mui/icons-material/Category";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import BookOnlineIcon from "@mui/icons-material/BookOnline";
+import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
 import {
   BookEdit,
   BookList,
   BookCreate,
   BookShow,
 } from "@/components/admin/books";
-// import dataProvider from "@/providers/data-provider";
-// import authProvider from "@/providers/auth-provider";
-// import { CommentList } from "@/components/admin/comments"
-// console.log(authProvider);
-//
+import {
+  UserCreate,
+  UserEdit,
+  UserList,
+  UserShow,
+} from "@/components/admin/users";
+import {
+  ProfileCreate,
+  ProfileEdit,
+  ProfileList,
+  ProfileShow,
+} from "@/components/admin/profiles";
+import {
+  AuthorCreate,
+  AuthorEdit,
+  AuthorList,
+  AuthorShow,
+} from "@/components/admin/authors";
+import {
+  CategoryCreate,
+  CategoryEdit,
+  CategoryList,
+  CategoryShow,
+} from "@/components/admin/categories";
+import {
+  BorrowCreate,
+  BorrowEdit,
+  BorrowList,
+  BorrowShow,
+} from "@/components/admin/borrows";
+import {
+  CommentCreate,
+  CommentEdit,
+  CommentList,
+  CommentShow,
+} from "@/components/admin/comments";
+import { Dashboard } from "@/components/admin/dashboard";
 import drfProvider from "@/providers/drf-provider";
-import jwtTokenAuthProvider, { fetchJsonWithAuthJWTToken } from "@/providers/auth-provider";
-import { UserCreate, UserEdit, UserList, UserShow } from "@/components/admin/users";
-import { ProfileCreate, ProfileEdit, ProfileList, ProfileShow } from "@/components/admin/profiles";
-import { AuthorCreate, AuthorEdit, AuthorList, AuthorShow } from "./components/admin/authors";
-import { CategoryCreate, CategoryEdit, CategoryList, CategoryShow } from "./components/admin/categories";
-import { BorrowCreate, BorrowEdit, BorrowList, BorrowShow } from "./components/admin/borrows";
-import { CommentCreate, CommentEdit, CommentList, CommentShow } from "./components/admin/comments";
+import jwtTokenAuthProvider, {
+  fetchJsonWithAuthJWTToken,
+} from "@/providers/auth-provider";
 
 const apiUrl = import.meta.env.VITE_API_URL as string;
-const authProvider = jwtTokenAuthProvider({ obtainAuthTokenUrl: `${apiUrl}/token/`, refreshTokenUrl: `${apiUrl}/token/refresh/` })
+const authProvider = jwtTokenAuthProvider({
+  obtainAuthTokenUrl: `${apiUrl}/token/`,
+  refreshTokenUrl: `${apiUrl}/token/refresh/`,
+});
 const dataProvider = drfProvider(apiUrl, fetchJsonWithAuthJWTToken);
 
 function App() {
   return (
-    <Admin authProvider={authProvider} dataProvider={dataProvider} requireAuth>
+    <Admin
+      authProvider={authProvider}
+      dataProvider={dataProvider}
+      dashboard={Dashboard}
+      requireAuth
+    >
       <Resource
         name="users"
         list={UserList}
