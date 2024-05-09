4c6572e00d54b65c0e2bd5d5569657c14048047f
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-05-09 03:01:31 +0300

    refactor: moved BookList to its own file

---
+++ client/src/components/library/books/book-list.tsx
@@ -0,0 +1,29 @@
+import { Box, Grid } from "@mui/material";
+import { ListBase, Pagination, useListContext } from "react-admin";
+import Book from "./book";
+
+export function BookListContent() {
+  const { data: books } = useListContext();
+  return (
+    <Grid container spacing={3}>
+      {books && books.map((book) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} className="flex justify-center">
+          <Book key={book.id} book={book} />
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+
+function BookList() {
+  return (
+    <ListBase>
+      <Box className="my-5">
+        < BookListContent />
+        <Pagination />
+      </Box>
+    </ListBase >
+  );
+}
+
+export default BookList;
--- client/src/components/library/index.tsx
+++ client/src/components/library/index.tsx
@@ -1,49 +1,16 @@
 import {
   Admin,
-  CustomRoutes,
-  List,
-  ListBase,
-  Pagination,
   Resource,
-  useListContext,
 } from "react-admin";
 import Layout from "./layout";
 import { Route } from "react-router-dom";
-import Book from "./books/book";
-import { Box, Grid } from "@mui/material";
-import { useEffect } from "react";
-
-function BookListContent() {
-  const { data: books, isLoading } = useListContext();
-  return (
-    <Grid container spacing={3}>
-      {books && books.map((book) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} className="flex justify-center">
-          <Book key={book.id} book={book} />
-        </Grid>
-      ))}
-    </Grid>
-  )
-
-}
-
-function BookList() {
-  return (
-    <ListBase>
-      <Box className="my-5">
-        < BookListContent />
-        <Pagination />
-      </Box>
-    </ListBase >
-  );
-}
+import BookList from "./books/book-list";
 
 function BookItem() {
   return (
     <h1>Book item</h1>
   );
 }
-
 
 function Library({ dataProvider, authProvider }) {
   return (
