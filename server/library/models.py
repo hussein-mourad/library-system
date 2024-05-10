758bd3c3a51c1986b5703219a7d3f9f3f6526018
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2024-05-10 03:40:15 +0300

    fix: can't updated borrow date

--- server/library/models.py
+++ server/library/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.timezone import timezone
@@ -61,7 +63,7 @@ class Book(models.Model):
 class Borrow(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    borrow_date = models.DateTimeField(auto_now_add=True)
+    borrow_date = models.DateTimeField(null=False, blank=False, default=datetime.now)
     return_date = models.DateTimeField(null=True, blank=True)
     returned = models.BooleanField(default=False)
 
